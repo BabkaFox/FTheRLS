d1d1b03d8cd0909c9867e61e607618782e5f681d
Babka Fox <chemaxrus1@gmail.com>
2014-12-13 00:45:13 +0300

    Исправлено направление объекта в сторону РЛС

--- MoveSObject.cpp
+++ MoveSObject.cpp
@@ -18,39 +18,6 @@ MoveSObject::MoveSObject(SObject obj, RLS rls) {
     vSObject.push_back(obj); //получили один готовый объект(тело) сразу поместили его в массив перемещений
 //скорее всего в комментариях сказано не совсем то, что происходит на самом деле. например "вектор тела" может быть не вектором тела
 
-/*    //вектор тела
-    double vecObjX = obj.getObjX();
-    double vecObjY = obj.getObjY();
-//    double vecObjZ = obj.getObjZ();
-
-    //вектор рлс
-    double vecRlsX = rls.getXrls() - vecObjX;
-    double vecRlsY = rls.getYrls() - vecObjY;
-//    double vecRlsZ = rls.getZrls() - vecObjZ;
-
-    //нормаль вектора объкта
-    double vecObjXNorm = vecObjX/sqrt(vecObjX*vecObjX+vecObjY*vecObjY);
-    double vecObjYNorm = vecObjY/sqrt(vecObjX*vecObjX+vecObjY*vecObjY);
-//    double vecObjXNorm = vecObjX/sqrt(vecObjX*vecObjX+vecObjY*vecObjY+vecObjZ*vecObjZ);
-//    double vecObjYNorm = vecObjY/sqrt(vecObjX*vecObjX+vecObjY*vecObjY+vecObjZ*vecObjZ);
-//    double vecObjZNorm = vecObjZ/sqrt(vecObjX*vecObjX+vecObjY*vecObjY+vecObjZ*vecObjZ);
-
-    //нормаль вектора РЛС
-    double vecRlsXNorm = vecRlsX/sqrt(vecRlsX*vecRlsX+vecRlsY*vecRlsY);
-    double vecRlsYNorm = vecRlsY/sqrt(vecRlsX*vecRlsX+vecRlsY*vecRlsY);
-//    double vecRlsXNorm = vecRlsX/sqrt(vecRlsX*vecRlsX+vecRlsY*vecRlsY+vecRlsZ*vecRlsZ);
-//    double vecRlsYNorm = vecRlsY/sqrt(vecRlsX*vecRlsX+vecRlsY*vecRlsY+vecRlsZ*vecRlsZ);
-//    double vecRlsZNorm = vecRlsZ/sqrt(vecRlsX*vecRlsX+vecRlsY*vecRlsY+vecRlsZ*vecRlsZ);
-
-    //Расчитываем угол направления движения к РЛС
-    double theta = acos(vecObjXNorm*vecRlsXNorm+vecObjYNorm*vecRlsYNorm);
-//    double theta = acos(vecObjXNorm*vecRlsXNorm+vecObjYNorm*vecRlsYNorm+vecObjZNorm*vecRlsZNorm);
-
-    //переводим в градусы
-    double degTheta = theta*180/M_PI;
-    std::cout<<degTheta;
-    */
-
     //вектор тела
     double vecObjX = obj.getObjX();
     double vecObjY = obj.getObjY();
@@ -63,25 +30,30 @@ MoveSObject::MoveSObject(SObject obj, RLS rls) {
     //гипотенуза
     double gipgip = sqrt(vecRlsX*vecRlsX+vecRlsY*vecRlsY);
 
-    //находи угол в градусах
+    //находим угол в градусах
     double degTheta = acos(vecRlsX/gipgip)*180/M_PI;
 
 //начинаем двигать цель
     double  Vx=0,Vy=0,Vz=0;
 
 
-    Vx = obj.getVi()*cos(obj.getGamma()+degTheta);   //курс
-    Vy = obj.getVi()*sin(obj.getGamma()+degTheta);
+//    Vx = obj.getVi()*cos(obj.getGamma()+degTheta);   //курс
+//    Vy = obj.getVi()*sin(obj.getGamma()+degTheta);
     Vz = 0;
+
+    Vx = obj.getVi()*cos(degTheta * (M_PI /180));
+    Vy = obj.getVi()*sin(degTheta * (M_PI /180));
+
+    std::cout<<Vx<<" "<<Vy<<" "<<Vz<<std::endl;
     int tend = 1000;
     for (int t = 30; t < tend; t+=30) {
 
-        double x = (Vx*t+vSObject[t/30-1].getObjX());
-        double y = (Vy*t+vSObject[t/30-1].getObjY());
-        double z = (Vz*t+vSObject[t/30-1].getObjZ());
+//        double x = (Vx*t+vSObject[t/30-1].getObjX());
+//        double y = (Vy*t+vSObject[t/30-1].getObjY());
+        double x = Vx * 1 + vSObject[t/30-1].getObjX();
+        double y = Vy * 1 + vSObject[t/30-1].getObjY();
 
-//        double x = cos(degTheta * (M_PI /180)) * t * obj.getVi() + vSObject[t/30-1].getObjX();
-//        double y = sin(degTheta * (M_PI /180)) * t * obj.getVi() + vSObject[t/30-1].getObjY();
+        double z = (Vz*t+vSObject[t/30-1].getObjZ());
 //        std::cout<<"X: "<<x<<" Y: "<<y<<" Z: "<<z<<std::endl;
         //Содаем и заносим объекты
         vSObject.push_back(SObject(obj.getN(),obj.getVi(),obj.getGamma(),t,x,y,z,obj.getSizeObjX(),obj.getSizeObjX(),obj.getSizeObjX(),obj.getVPoint()));
